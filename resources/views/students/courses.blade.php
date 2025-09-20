8b53a4fd13358c6cfd9f013c8e110e81d93cc18a
rianphlox <51505096+rianphlox@users.noreply.github.com>
2025-09-20 23:52:49 +0100

    Update courses.blade.php

--- resources/views/students/courses.blade.php
+++ resources/views/students/courses.blade.php
@@ -2,88 +2,62 @@
 
 @section('content')
 <div class="row">
-
-          
     <div class="col-md-12">
-      <div >
-        <ol class="breadcrumb">
-          
-          <li class="breadcrumb-item"><a href="">Tables</a></li>
-          <li class="breadcrumb-item active" aria-current="page">Student</li>
-        </ol>
-      </div>
-      <div class="card">
-        <div class="card-header">
-          <h4 class="card-title">Showing Registered Courses for PG/{{ $mat }}</h4>
+        <div>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Tables</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Student</li>
+            </ol>
         </div>
-        <div class="card-body">
-          <div class="table-responsive">
-            <table class="table">
-              
-              @if (!count($courses) )
-                <thead class="text-primary">
-                  <th class="th-sm">Course Code</th>
-                  <th class="th-sm">Course Name</th>
-                  <th class="">Year</th>
-                  <th class="">Semester</th>
-                  <th class="">Grade</th>
-                  <th class=""></th>
-                  <th class=""></th>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>No Registered Courses</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                    <a href='{{ route('register', [$mat, $programme]) }}' class="btn btn-small btn-warning">
-                        <span>Register</span>
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              
 
-                @else
-                  <thead>
-                  <th class="th-sm">Course Code</th>
-                      <th class="th-sm">Course Name</th>
-                      <th class="">Year</th>
-                      <th class="">Semester</th>
-                      <th class="">Grade</th>
-                      <th class="">Score</th>
-                      <th class=""></th>
-                  </thead>
-                  <tbody>
-                    @foreach($courses as $course)
-                      
-                        <tr>
-                          <td><?= "PRE$course->course_code"; ?></td>
-                          <td>{{ $course->course_name }}</td>
-                          <td> <center>{{ explode('.', $course->course_id)[0] }}</center> </td>
-                          <td> <center>{{ explode('.', $course->course_id)[1] }}</center> </td>
-                          <td><center>{{ strtoupper($course->grade) }}</center></td>
-                          <td><center> {{ $course->score }} </center></td>
-                          <td class="_remark">
-                            <a href="" type="button" rel="tooltip" class="btn btn-small btn-info">
-                              <!-- <i class="material-icons">edit</i> -->
-                              <span>Edit</span>
-                            </a>
-                          </td>
-                        </tr>
-                    <?php endforeach; ?>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Showing Registered Courses for PG/{{ $mat }}</h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead class="text-primary">
+                            <tr>
+                                <th>Course Code</th>
+                                <th>Course Name</th>
+                                <th>Year</th>
+                                <th>Semester</th>
+                                <th>Grade</th>
+                                <th>Score</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
 
-                  </tbody>
-                @endif
-                
-            </table>
-          </div>
+                        <tbody>
+                            @forelse ($courses as $course)
+                                <tr>
+                                    <td>{{ "PRE{$course->course_code}" }}</td>
+                                    <td>{{ $course->course_name }}</td>
+                                    <td class="text-center">{{ explode('.', $course->course_id)[0] }}</td>
+                                    <td class="text-center">{{ explode('.', $course->course_id)[1] }}</td>
+                                    <td class="text-center">{{ strtoupper($course->grade) }}</td>
+                                    <td class="text-center">{{ $course->score }}</td>
+                                    <td>
+                                        <a href="#" class="btn btn-sm btn-info">Edit</a>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6">No Registered Courses</td>
+                                    <td>
+                                        <a href="{{ route('register', [$mat, $programme]) }}" 
+                                           class="btn btn-sm btn-warning">
+                                            Register
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-    
-  </div>
+</div>
 @endsection
